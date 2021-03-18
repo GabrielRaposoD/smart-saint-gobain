ed717d8f0de75a406fb35e02804d3a40dc0cf242
Davi Duarte <=>
2021-03-18 01:43:05 -0300

    Added logic for radio button choose and fixed input type=radio

--- src/components/RadioInput/index.tsx
+++ src/components/RadioInput/index.tsx
@@ -1,36 +1,33 @@
 // Module Imports
-import React, { InputHTMLAttributes } from 'react';
-import { Field, ErrorMessage } from 'formik';
-import cs from 'classnames';
+import React, { InputHTMLAttributes } from 'react'
+import { Field, ErrorMessage } from 'formik'
+import cs from 'classnames'
 
 interface RadioInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const RadioInput: React.FC<RadioInputProps> = ({ id, name, value }) => {
   return (
-    <Field name={name} type='radio' value={value}>
+    <Field name={name} type="radio" value={value}>
       {({ field, meta }) => (
-        <div className='flex flex-col'>
+        <div className="flex flex-col">
           <input
             name={name}
-            type='radio'
-            value={value}
+            type="radio"
             className={cs(' ', {
-              'border-red-500 hover:border-red-500 focus:border-red-500':
-                meta.error,
+              'border-red-500 hover:border-red-500 focus:border-red-500': meta.error,
               'border-shift hover:border-green focus:border-green': !meta.error,
             })}
-            {...field}
+            onChange={field.onChange}
+            value={value}
+            id={String(value)}
           />
-          <p
-            data-testid={`${id}-errorlabel`}
-            className={cs('mt-2 text-sm text-red-500')}
-          >
+          <p data-testid={`${id}-errorlabel`} className={cs('mt-2 text-sm text-red-500')}>
             <ErrorMessage name={name} />
           </p>
         </div>
       )}
     </Field>
-  );
-};
+  )
+}
 
-export { RadioInput };
+export { RadioInput }
--- src/components/Steps/TrackSelect/index.tsx
+++ src/components/Steps/TrackSelect/index.tsx
@@ -1,46 +1,48 @@
 // Module Imports
-import React from 'react';
+import React from 'react'
+import { useFormikContext } from 'formik'
+
+// Application Import
+import { ButtonState, SmartStep } from '@typings/index'
+import { tracks } from '@mocks/tracks'
 
 // Components Import
-import { Logo, Stepper } from '@components/index';
-import { ButtonState, SmartStep } from '@typings/index';
-import { tracks } from '@mocks/tracks';
-import { RadioInput } from '@components/RadioInput';
-import { Field } from 'formik';
+import { Logo, Stepper } from '@components/index'
+import { RadioInput } from '@components/RadioInput'
 
 const TrackSelect: SmartStep = () => {
+  const formik = useFormikContext<any>()
+
   return (
-    <div className='md:px-0 md:min-h-0 md:pb-0 flex flex-col items-start justify-between h-full min-h-screen px-6 pb-10'>
-      <div className='flex flex-col'>
-        <div className='md:mt-0 mt-6'>
+    <div className="md:px-0 md:min-h-0 md:pb-0 flex flex-col items-start justify-between h-full min-h-screen px-6 pb-10">
+      <div className="flex flex-col">
+        <div className="md:mt-0 mt-6">
           <Logo />
         </div>
-        <div className='mt-16'>
-          <h1 className='md:text-4xl text-2xl font-bold leading-snug text-gray-800'>
+        <div className="mt-16">
+          <h1 className="md:text-4xl text-2xl font-bold leading-snug text-gray-800">
             Escolha uma trilha sonora para o seu anúncio
           </h1>
-          <h3 className='mt-3 text-base font-medium text-gray-600'>
+          <h3 className="mt-3 text-base font-medium text-gray-600">
             Escolha nas opções abaixo o que melhor te atende.
           </h3>
-          <div className='mt-6 text-base font-medium text-gray-600'></div>
+          <div className="mt-6 text-base font-medium text-gray-600"></div>
           {tracks.map((t, i) => (
-            <div className='flex flex-col mt-4 space-y-1'>
-              <div className='flex flex-row space-x-1'>
-                <RadioInput name='track' id='track' value={t.id} />
-                <label className='self-center font-medium'>
-                  Trilha Sonora {i + 1}
-                </label>
+            <div className="flex flex-col mt-4 space-y-1">
+              <div className="flex flex-row space-x-1">
+                <RadioInput name="track" id="track" value={t.id} />
+                <label className="self-center font-medium">Trilha Sonora {i + 1}</label>
               </div>
               <audio controls src={t.url} />
             </div>
           ))}
         </div>
       </div>
-      <div className='w-full mt-6'>
-        <Stepper buttonState={ButtonState.normal} />
+      <div className="w-full mt-6">
+        <Stepper buttonState={formik.values.track !== null ? ButtonState.normal : ButtonState.disabled} />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export { TrackSelect };
+export { TrackSelect }
